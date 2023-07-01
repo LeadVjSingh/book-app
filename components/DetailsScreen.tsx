674f25d27fed37aa1c05e3b1e7193eb99547652a
LeadVjSingh <42995488+LeadVjSingh@users.noreply.github.com>
2023-07-01 23:18:46 +0530

    Update DetailsScreen.tsx

--- components/DetailsScreen.tsx
+++ components/DetailsScreen.tsx
@@ -1,14 +1,41 @@
-import { View, Button, Text } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { FlatList, SafeAreaView, ScrollView, StyleSheet, Text, View, Image, RefreshControl, ActivityIndicator, Button, TouchableNativeFeedback, TouchableHighlight } from 'react-native';
+import myItemSeparator from './myItemSeparator';
+import myListEmpty from './myListEmpty';
+import { useState, useEffect } from 'react';
 
 export default function DetailsScreen({ route }) {
-    console.log("here is the props", route.params)
+    // console.log("here is the props", route.params)
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>{route.params.title} Hello</Text>
+        <View>
 
-            <Button title="Go to 자유톡" onPress={() => route.navigate('BookList')} />
-            <Button title="Go back" onPress={() => route.goBack()} />
+            <View style={styles.container}>
+
+                <View style={{ backgroundColor: '#529FF3', margin: 10 }}>
+                    <Image source={{ uri: route.params.item.src ?? '' }} style={{ height: 100, width: 100, alignSelf: 'center' }} />
+                </View>
+                <View style={{ alignItems: 'stretch', flexDirection: 'row' }}>
+                    <Text style={{ paddingVertical: 10, fontSize: 15, paddingStart: 5, paddingEnd: 16, color: 'black' }}>
+                        {route.params.item.title ?? ''} </Text>
+                    <Text style={{ paddingVertical: 10, fontSize: 15, paddingStart: 5, paddingEnd: 16, color: 'black' }}>
+                        {route.params.item.price ?? ''} 원</Text>
+                </View>
+
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 50,
+
+    },
+    item: {
+        padding: 20,
+        fontSize: 15,
+        marginTop: 5,
+    }
+})
